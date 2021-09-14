1ca456e38b081ab9725051b5bbf802ff2adf1bbc
Ruthenium <rutaspk@gmail.com>
2021-09-14 17:41:41 +0200

    Some progress but broken

--- Assets/Scripts/Easing.cs
+++ Assets/Scripts/Easing.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Easing : MonoBehaviour
 {
-    private enum EaseStyle
+    public enum EaseStyle
     {
         EaseLinear,
         EaseInSine,
@@ -29,196 +30,59 @@ public class Easing : MonoBehaviour
         EaseOutQuint,
         EaseInOutQuint
     }
-    [SerializeField] private EaseStyle easeStyle;
-
-    delegate float EaseFunction(float a, float b, float t);
+    public delegate float EaseFunction(float a, float b, float t);
     private EaseFunction easeFunction;
     
-    [SerializeField] private Vector3 start;
-    [SerializeField] private Vector3 end;
+    private float start;
+    private float end;
 
+    public float Start1
+    {
+        set => start = value;
+    }
+
+    public float End
+    {
+        set => end = value;
+    }
     void Start()
     {
-        SetEase(easeFunction);
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(EaseSelected(easeFunction, start, end));
-        }
-    }
-
-    private IEnumerator EaseSelected(EaseFunction ease,Vector3 startPos, Vector3 endPos)
-    {
-        float t = 0;
-        while (t < 1.0f)
-        {
-            transform.position = Ease(ease, startPos, endPos, t);
-            t += Time.unscaledDeltaTime;
-            yield return null;
-        }
-        transform.position = endPos;
-    }
-
-    float EaseLinear(float a, float b, float t)
-    {
-        return a * (1 - t) + b * t;
+        //easeFunction = SetEase(easeStyle);
     }
     
-    float EaseInSine(float a, float b, float t)
-    {
-        t = 1 - Mathf.Cos(t * Mathf.PI / 2);
-        return EaseLinear(a, b, t);
-    }
-    
-    float EaseOutSine(float a, float b, float t)
-    {
-        t = Mathf.Sin(t * Mathf.PI / 2);
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseInOutSine(float a, float b, float t)
-    {
-        t = (1 + Mathf.Sin(((t - 0.5f) * Mathf.PI))) / 2;
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseInCircular(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-    
-    float EaseOutCircular(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-    
-    float EaseInOutCircular(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t); 
-    }
-    
-    float EaseInExpo(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-    
-    float EaseOutExpo(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-    
-    float EaseInOutExpo(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-    
-    float EaseInQuad(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseOutQuad(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseInOutQuad(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseInCub(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseOutCub(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-    
-    float EaseInOutCub(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseInQuart(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseOutQuart(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseInOutQuart(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseInQuint(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseOutQuint(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    float EaseInOutQuint(float a, float b, float t)
-    {
-        return EaseLinear(a, b, t);
-    }
-
-    
-    //converters
-    private float Ease(EaseFunction ease, float a, float b, float t)
-    {
-        return ease(a, b, t);
-    }
-    
-    private Vector2 Ease(EaseFunction ease, Vector2 a, Vector2 b, float t)
-    {
-        return new Vector2(ease(a.x, b.x, t), ease(a.y, b.y, t));
-    }
-    
-    private Vector3 Ease(EaseFunction ease,Vector3 a, Vector3 b, float t)
-    {
-        return new Vector3(ease(a.x, b.x, t), ease(a.y, b.y, t), ease(a.z, b.z, t));
-    }
-
-    private EaseFunction SetEase(EaseFunction ease)
+    public void SetEase(EaseStyle easeStyle)
     {
         switch (easeStyle)
         {
             case EaseStyle.EaseLinear:
-                ease = EaseLinear;
+                easeFunction = EaseLinear;
                 break;
             case EaseStyle.EaseInSine:
-                ease = EaseInSine;
+                easeFunction = EaseInSine;
                 break;
             case EaseStyle.EaseOutSine:
-                ease = EaseOutSine;
+                easeFunction = EaseOutSine;
                 break;
             case EaseStyle.EaseInOutSine:
-                ease = EaseInOutSine;
+                easeFunction = EaseInOutSine;
                 break;
             case EaseStyle.EaseInCircular:
+                easeFunction = EaseInCircular;
                 break;
             case EaseStyle.EaseOutCircular:
+                easeFunction = EaseOutCircular;
                 break;
             case EaseStyle.EaseInOutCircular:
+                easeFunction = EaseInOutCircular;
                 break;
             case EaseStyle.EaseInExpo:
+                easeFunction = EaseInExpo;
                 break;
             case EaseStyle.EaseOutExpo:
+                easeFunction = EaseOutExpo;
                 break;
             case EaseStyle.EaseInOutExpo:
+                easeFunction = EaseInOutExpo;
                 break;
             case EaseStyle.EaseInQuad:
                 break;
@@ -247,6 +111,175 @@ public class Easing : MonoBehaviour
             default:
                 throw new ArgumentOutOfRangeException();
         }
-        return ease;
+    }
+
+    //using this stuff??
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            StartCoroutine(EaseSelected(easeFunction, start, end));
+        }
+    }
+
+    private IEnumerator EaseSelected(EaseFunction ease, float startPos, float endPos)
+    {
+        float t = 0;
+        while (t < 1.0f)
+        {
+            transform.position = new Vector3(transform.position.x, Ease(ease, startPos, endPos, t, 1), transform.position.z);
+            t += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        transform.position = new Vector3(transform.position.x, endPos, transform.position.z);
+    }
+    
+    //converters
+    private static float Ease(EaseFunction ease, float a, float b, float t, float d)
+    {
+        return ease(a, b, t / d);
+    }
+    
+    private Vector2 Ease(EaseFunction ease, Vector2 a, Vector2 b, float t, float d)
+    {
+        return new Vector2(ease(a.x, b.x, t / d), ease(a.y, b.y, t / d));
+    }
+    
+    private Vector3 Ease(EaseFunction ease,Vector3 a, Vector3 b, float t, float d)
+    {
+        return new Vector3(ease(a.x, b.x, t / d), ease(a.y, b.y, t / d), ease(a.z, b.z, t / d));
+    }
+
+    //the easings
+    float EaseLinear(float a, float b, float t)
+    {
+        return a * (1 - t) + b * t;
+    }
+    
+    float EaseInSine(float a, float b, float t)
+    {
+        t = 1 - Mathf.Cos(t * Mathf.PI / 2);
+        return EaseLinear(a, b, t);
+    }
+    
+    float EaseOutSine(float a, float b, float t)
+    {
+        t = Mathf.Sin(t * Mathf.PI / 2);
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseInOutSine(float a, float b, float t)
+    {
+        t = (1 + Mathf.Sin(((t - 0.5f) * Mathf.PI))) / 2;
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseInCircular(float a, float b, float t)
+    {
+        t = 1 - Mathf.Sqrt(1 - t * t);
+        return EaseLinear(a, b, t);
+    }
+    
+    float EaseOutCircular(float a, float b, float t)
+    {
+        t = Mathf.Sqrt(1 - Mathf.Pow(2, (t - 1)));
+        return EaseLinear(a, b, t);
+    }
+    
+    float EaseInOutCircular(float a, float b, float t)
+    {
+        t = t <= 0.5f ? 0.5f - Mathf.Sqrt(0.25f - t * t) : Mathf.Sqrt(0.25f - Mathf.Pow(2, (t - 1))) + 0.5f;
+        return EaseLinear(a, b, t);
+    }
+    
+    float EaseInExpo(float a, float b, float t)
+    {
+        t = 1 - Mathf.Sqrt(1 - t);
+        return EaseLinear(a, b, t);
+    }
+    
+    float EaseOutExpo(float a, float b, float t)
+    {
+        t = Mathf.Sqrt(t);
+        return EaseLinear(a, b, t);
+    }
+    
+    float EaseInOutExpo(float a, float b, float t)
+    {
+        t = t <= 0.5f ? Mathf.Sqrt(2 * t - 1) / 2 + 0.5f : 0.5f - Mathf.Sqrt(1 - 2 * t) / 2;
+        return EaseLinear(a, b, t);
+    }
+    
+    float EaseInQuad(float a, float b, float t)
+    {
+        t = t * t;
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseOutQuad(float a, float b, float t)
+    {
+        t = 1 - Mathf.Pow(2, t - 1);
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseInOutQuad(float a, float b, float t)
+    {
+        t = t <= 0.5f ? 1 - Mathf.Pow(2, 2 * t - 2) / 2 : 2 * t * t;
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseInCub(float a, float b, float t)
+    {
+        t = t * t * t;
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseOutCub(float a, float b, float t)
+    {
+        t = Mathf.Pow(3, t - 1) + 1;
+        return EaseLinear(a, b, t);
+    }
+    
+    float EaseInOutCub(float a, float b, float t)
+    {
+        t = t <= 0.5 ? 4 * t * t * t : 4 * Mathf.Pow(3, t - 1) + 1;
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseInQuart(float a, float b, float t)
+    {
+        t = Mathf.Pow(4, t);
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseOutQuart(float a, float b, float t)
+    {
+        t = Mathf.Pow(4, t - 2) + 2;
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseInOutQuart(float a, float b, float t)
+    {
+        t = t <= 0.5f ? - 8 * Mathf.Pow(4, t - 0.5f) + 0.5f : 8 * Mathf.Pow(4, t - 0.5f) + 0.5f;
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseInQuint(float a, float b, float t)
+    {
+        t = Mathf.Pow(5, t);
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseOutQuint(float a, float b, float t)
+    {
+        t = Mathf.Pow(5, t - 1) + 1;
+        return EaseLinear(a, b, t);
+    }
+
+    float EaseInOutQuint(float a, float b, float t)
+    {
+        t = t <= 0.5f ? 16 * Mathf.Pow(5, t) : 16 * Mathf.Pow(5, t - 1) + 1;;
+        return EaseLinear(a, b, t);
     }
 }
---
+++ Assets/Scripts/SpawnBalls.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalls : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;
+    
+    void Start()
+    {
+        float offsetX = 5.5f;
+        float offsetY = 0;
+        float paddingY = 4;
+        for (int x = 0; x < 12; x++)
+        {
+            for (int y = 0; y < 2; y++)
+            {
+                GameObject temp = Instantiate(ballPrefab, transform);
+                temp.transform.position =
+                    new Vector3(transform.position.x + x - offsetX, transform.position.y + y * paddingY - offsetY, transform.position.z);
+                float num = y * paddingY - offsetY;
+                temp.GetComponent<Easing>().Start1 = num;
+                temp.GetComponent<Easing>().End = num - 4;
+                temp.GetComponent<Easing>().SetEase((Easing.EaseStyle) (x * 2 + y));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
